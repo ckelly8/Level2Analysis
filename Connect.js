24c4ba017703bf69cbf9adc9e124a90ace793c71
ckelly8 <ckelly2055@gmail.com>
2023-12-04 21:22:41 -0500

    Refactoring of code base for javascript based cloud deployment.

--- Connect.js
+++ Connect.js
@@ -1,58 +1,21 @@
 const WebSocket = require('ws');
 
-const websocketURL = 'wss://ws-feed.exchange.coinbase.com'; // Coinbase WebSocket URL for the public feed
-const pollingURL = 'ws://localhost:8080' // Local port feed to read from
+const ws = new WebSocket('wss://ws-feed.exchange.coinbase.com', {
+  perMessageDeflate: true
+});
 
-let ws;
+const subscribeMessage = JSON.stringify({ type: 'subscribe', product_ids: ['BTC-USD'], channels: ['level2','heartbeat',{name : 'ticker', product_ids: ['BTC-USD']}]})
 
-// Trading pairs map
-// Can add additional pairs as desired
-const tradingPairs = {
-  'BTC-USD': {
-    name: 'BTC-USD',
-  }
-};
+ws.on('open', function open() {
+  console.log('Connected');
+  ws.send(subscribeMessage);
+});
 
-const subscribeMessage = {
-  type: 'subscribe',
-  product_ids: Object.keys(tradingPairs), // Subscribe to all trading pairs
-  channels: ['level2', 'ticker']
-};
+ws.on('message', function incoming(data) {
+  
+  console.log(JSON.parse(data));
+});
 
-function connect() {
-  ws = new WebSocket(websocketURL);
-  const ps = new WebSocket(pollingURL)
-
-  // Event: Connection established
-  ws.on('open', () => {
-    console.log('Connected to Coinbase WebSocket feed');
-
-    // Subscribe to Level 2 order book feed
-    ws.send(JSON.stringify(subscribeMessage));
-  });
-
-  // Event: Message received
-  ws.on('message', (data) => {
-    const message = JSON.parse(data);
-    ps.send(JSON.stringify(message))
-  });
-
-  // Event: Connection closed
-  ws.on('close', () => {
-    console.log('Connection closed');
-  });
-
-  // Event: Error occurred
-  ws.on('error', (error) => {
-    console.error('WebSocket error:', error);
-  });
-}
-
-// connect the first time
-connect();
-
-// close and reopen connection every 10 minutes
-setInterval(() => {
-  ws.close();
-  connect();
-}, 600000);
+ws.on('error', function error(error) {
+  console.error('WebSocket error:', error);
+});

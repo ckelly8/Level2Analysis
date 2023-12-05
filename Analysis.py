cf383aedbe8791c074b3bb1e729f7933df388090
ckelly8 <ckelly2055@gmail.com>
2023-12-04 19:21:39 -0500

    Deprecate old python files

--- Analysis.py
+++
@@ -1,6 +0,0 @@
-def calculate_center_of_mass(ob_array):
-    distances = ob_array[:, 0]
-    masses = ob_array[:, 1]
-    total_mass = np.sum(masses)
-    center_of_mass = np.sum(distances * masses) / total_mass
-    return center_of_mass
--- Run_Event.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start "Record" cmd /k "Python Record.py"
-timeout /t 2 >nul
-set NODE_OPTIONS=--max-old-space-size=16384
-start "Connector" cmd /k "node Connect.js"

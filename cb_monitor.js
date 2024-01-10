4e2e01f42b961527d6e36ee573a8c08559c100f3
ckelly8 <ckelly2055@gmail.com>
2024-01-09 19:42:32 -0500

    added functionality to handle disconnect from websocket

--- cb_monitor.js
+++ cb_monitor.js
@@ -9,26 +9,59 @@ const ob = new OrderBook('BTC-USD');
 recordSizeArray = [1,10,50,100,250,500,1000,2500,5000,'full'];
 const recordObject = new OrderBookRecordObject(ob, recordSizeArray);
 
-//connect to websocket
-const ws = new WebSocket('wss://ws-feed.exchange.coinbase.com', {
-  perMessageDeflate: true
-});
+function connectWebSocket() {
+  const ws = new WebSocket('wss://ws-feed.exchange.coinbase.com', {
+    perMessageDeflate: true
+  });
 
-const subscribeMessage = JSON.stringify({ type: 'subscribe', product_ids: ['BTC-USD'], channels: ['level2_batch',{name : 'ticker', product_ids: ['BTC-USD']}]});
+  const subscribeMessage = JSON.stringify({ 
+    type: 'subscribe', 
+    product_ids: ['BTC-USD'], 
+    channels: ['level2_batch', { name: 'ticker', product_ids: ['BTC-USD'] }]
+  });
 
-ws.on('open', function open() {
-  console.log('Connected');
-  ws.send(subscribeMessage);
-});
+  let pingInterval;
 
-//read websocket data
-ws.on('message', function incoming(data) {
-  ob.readDataStream(JSON.parse(data));
-});
+  ws.on('open', function open() {
+    console.log('Connected');
+    ws.send(subscribeMessage);
+    startHeartbeat();
+  });
 
-ws.on('error', function error(error) {
-  console.error('WebSocket error:', error);
-});
+  ws.on('message', function incoming(data) {
+    ob.readDataStream(JSON.parse(data));
+    // Reset the ping interval on any message received
+    clearInterval(pingInterval);
+    startHeartbeat();
+  });
+
+  ws.on('error', function error(error) {
+    console.error('WebSocket error:', error);
+    stopHeartbeat();
+  });
+
+  ws.on('close', function close() {
+    console.log('WebSocket closed. Reconnecting...');
+    stopHeartbeat();
+    setTimeout(connectWebSocket, 5000); // Reconnect after 5 seconds
+  });
+
+  function startHeartbeat() {
+    pingInterval = setInterval(function() {
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send('ping');
+      } else {
+        clearInterval(pingInterval);
+      }
+    }, 30000); // Send a ping every 30 seconds
+  }
+
+  function stopHeartbeat() {
+    clearInterval(pingInterval);
+  }
+}
+
+connectWebSocket();
 
 //record data after performing computations
 setInterval(() => {
@@ -39,6 +72,6 @@ setInterval(() => {
   }
 
   recordObject.record(ob);
-  //recordObject.displayRecordObject();
+  recordObject.displayRecordObject();
   //ob.displayOrderBook();
 }, 1000);

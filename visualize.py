2a0e1dd646d114c94ca215ddbfdeb3e03b232c80
Carson Kelly <CKelly@KPMFRANKLIN.LOCAL>
2024-01-11 12:56:33 -0500

    Reorganizing program layout in preparation of analysis components

--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 
-dataPath = os.path.join(os.getcwd(), 'data')
+dataPath = os.path.join(os.getcwd(), 'record/data')
 
 dataFileList = [f for f in os.listdir(dataPath) if f.endswith('.csv')]
 
@@ -17,9 +17,9 @@ df.set_index('time', inplace=True)
 plt.figure(figsize=(10, 6)) # Adjust the size of the plot
 
 # Plot the first value column, e.g., in blue
-plt.plot(df.index, df['50'], color='blue', label='COM 50')
-plt.plot(df.index, df['100'], color='green', label='COM 100')
-plt.plot(df.index, df['250'], color='black', label = 'COM 250')
+plt.plot(df.index, df['250'], color='blue', label='COM 50')
+plt.plot(df.index, df['500'], color='green', label='COM 100')
+plt.plot(df.index, df['1000'], color='black', label = 'COM 250')
 
 # Plot the second value column, e.g., in red
 plt.plot(df.index, df['price'], color='red', label='price')
